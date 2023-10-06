015b5ed5eab993708e369d21eb16d47c174b4154
Nathan Flynn <n8nflynn@gmail.com>
2023-10-06 16:22:38 +0100

    refactor code; add BookRecords struct

--- wych_book/src/io.rs
+++ wych_book/src/io.rs
@@ -1,33 +1,35 @@
 extern crate csv;
 
+use crate::book_record::{Book, BookRecords};
 use std::{error::Error, fs::File};
-use crate::book_record::BookRecord;
 
+// TODO: make this configurable
+pub const CSV_PATH: &'static str = "~/.config/wych_book/books.csv";
 
-pub fn read_csv_file(filename: &str) -> Result<Vec<BookRecord>, Box<dyn Error>> {
+pub fn read_csv_file(filename: &str) -> Result<BookRecords, Box<dyn Error>> {
     let file = File::open(filename)?;
     let mut reader = csv::Reader::from_reader(file);
-    let mut book_records = Vec::<BookRecord>::new();
+    let mut book_records = BookRecords::new();
 
     for result in reader.records() {
         let record = result?;
-        
+
         let author = record[0].to_string();
         let title = record[1].to_string();
         let weight: u8 = record[2].parse()?;
 
-        book_records.push(BookRecord::new(author, title, weight))
+        book_records.push(Book::new(author, title, weight))
     }
-    
+
     Ok(book_records)
 }
 
-pub fn write_csv_file(filename: &str, records: &Vec<BookRecord>) -> Result<(), Box<dyn Error>> {
+pub fn write_csv_file(filename: &str, books: &BookRecords) -> Result<(), Box<dyn Error>> {
     let mut writer = csv::Writer::from_path(filename)?;
 
-    writer.write_record(&BookRecord::headers())?;
+    writer.write_record(&Book::headers())?;
 
-    for record in records {
+    for record in books.records() {
         writer.write_record(&record.as_string_array())?;
     }
 
@@ -47,13 +49,16 @@ mod tests {
     fn test_reading_csv_file() {
         let filename = concat!(env!("CARGO_MANIFEST_DIR"), "/resources/test/books.csv");
         let result = read_csv_file(&filename).unwrap();
-        assert_eq!(4, result.len());
+        assert_eq!(4, result.records().len());
 
-        let record = BookRecord::new("Franz Kakfa".into(), 
-            "The Metamorphosis, and other stories".into(), 1);
-        assert_eq!(record, result[0]);
-        let record = BookRecord::new("Yōko Ogawa".into(), "The Memory Police".into(), 1);
-        assert_eq!(record, result[1]);
+        let record = Book::new(
+            "Franz Kakfa".into(),
+            "The Metamorphosis, and other stories".into(),
+            1,
+        );
+        assert_eq!(record, *result.get(0).unwrap());
+        let record = Book::new("Yōko Ogawa".into(), "The Memory Police".into(), 1);
+        assert_eq!(record, *result.get(1).unwrap());
     }
 
     #[test]
@@ -69,7 +74,7 @@ mod tests {
         writer.write_record(vec!["bad", "data"]).unwrap();
 
         let result = read_csv_file(filename).unwrap();
-        assert!(result.is_empty());
+        assert!(result.records().is_empty());
     }
 
     #[test]
@@ -78,12 +83,12 @@ mod tests {
         let file_path = temp_dir.path().join("new_books.csv");
         let filename = file_path.to_str().unwrap();
 
-        let records = vec![BookRecord::new("Franz Kakfa".into(), "The Castle".into(), 1)];
+        let records = vec![Book::new("Franz Kakfa".into(), "The Castle".into(), 1)].into();
         let result = write_csv_file(filename, &records);
         assert!(result.is_ok());
 
         let read_result = read_csv_file(filename).unwrap();
-        assert_eq!(read_result.len(), 1);
-        assert_eq!(read_result[0], records[0]);
+        assert_eq!(read_result.records().len(), 1);
+        assert_eq!(read_result.get(0), records.get(0));
     }
 }
