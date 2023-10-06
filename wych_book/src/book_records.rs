ba4123772a64afc2560fd192c8f4f4496f714986
Nathan Flynn <4656318+n8-n@users.noreply.github.com>
2023-10-06 17:22:22 +0100

    add add and remove book functions

--- wych_book/src/book_records.rs
+++ wych_book/src/book_records.rs
@@ -54,6 +54,32 @@ impl BookRecords {
             .flat_map(|(index, book)| vec![index; book.weight.into()])
             .collect()
     }
+
+    pub fn add_book(&mut self, author: &str, title: &str) {
+        self.push(Book { 
+            author: author.to_string(), 
+            title: title.to_string(), 
+            weight: 1 
+        });
+    }
+
+    /// Can remove book based on index, or title.
+    pub fn remove_book(&mut self, input: &str) {
+        let parse: Result<usize, _> = input.parse();
+        let records = &mut self.records;
+        let remove = |r: &mut Vec<Book>, i: usize| if i < r.len() { r.remove(i); };
+
+        if let Ok(index) = parse {
+            remove(records, index);
+            return;
+        }
+
+        // get index title
+        let result = records.iter().enumerate().find(|b| b.1.title == input);
+        if let Some(result) = result {
+            remove(records, result.0);
+        }
+    }
 }
 
 impl From<Vec<Book>> for BookRecords {
@@ -62,6 +88,10 @@ impl From<Vec<Book>> for BookRecords {
     }
 }
 
+
+// TODO: impl display
+
+
 //
 //
 //
@@ -69,26 +99,32 @@ impl From<Vec<Book>> for BookRecords {
 mod tests {
     use super::*;
 
+    fn books_to_test(weights: Vec<u8>) -> BookRecords {
+        vec![
+            Book::new("B. B. Author".into(), "A Title2".into(), weights[0]),
+            Book::new("A. Something Writer".into(), "The Title1".into(), weights[1]),
+            Book::new("C. A. Hack".into(), "Bad Title3".into(), weights[2]),
+        ]
+        .into()
+    }
+
+    // Can us weights as an easy way to check sorting order
+    fn collect_weights(books: &BookRecords) -> Vec<u8> {
+        books.records.iter().map(|b| b.weight).collect()
+    }
+
+
     #[test]
     fn test_weighted_index_vector() {
-        let books: BookRecords = vec![
-            Book::new("A. Writer".into(), "Title1".into(), 3),
-            Book::new("B. B. Author".into(), "Title2".into(), 2),
-            Book::new("C. A. Hack".into(), "Title3".into(), 0),
-        ]
-        .into();
+        let books: BookRecords = books_to_test(vec![3, 2, 0]);
         let result = books.weighted_index_vector();
         assert_eq!(result, vec![0, 0, 0, 1, 1])
     }
 
     #[test]
     fn test_random_book_selector() {
-        let books: BookRecords = vec![
-            Book::new("A. Writer".into(), "Title1".into(), 0),
-            Book::new("B. B. Author".into(), "Title2".into(), 5),
-            Book::new("C. A. Hack".into(), "Title3".into(), 0),
-        ]
-        .into();
+        let books: BookRecords = books_to_test(vec![0, 5, 0]);
+
         let result = books.select_random_book();
         // should always return second book
         assert_eq!(result.unwrap(), books.get(1).unwrap());
@@ -99,22 +135,26 @@ mod tests {
 
     #[test]
     fn test_book_records_sorting() {
-        let mut books: BookRecords = vec![
-            Book::new("B. B. Author".into(), "A Title2".into(), 5),
-            Book::new("A. Something Writer".into(), "The Title1".into(), 1),
-            Book::new("C. A. Hack".into(), "Bad Title3".into(), 4),
-        ]
-        .into();
-
-        // use weights as easy way to check sorting order
-        let weights_collect = |records: &BookRecords| -> Vec<u8> {
-            records.records.iter().map(|b| b.weight).collect()
-        };
+        let mut books: BookRecords = books_to_test(vec![5, 1, 4]);
 
         books.sort_by_author();
-        assert_eq!(weights_collect(&books), vec![1, 5, 4]);
+        assert_eq!(collect_weights(&books), vec![1, 5, 4]);
 
         books.sort_by_title();
-        assert_eq!(weights_collect(&books), vec![5, 4, 1]);
+        assert_eq!(collect_weights(&books), vec![5, 4, 1]);
+    }
+
+    #[test]
+    fn test_remove_book() {
+        let mut books: BookRecords = books_to_test(vec![3, 2, 0]);
+        books.remove_book("1");
+        books.remove_book("18"); // does nothing
+        assert_eq!(collect_weights(&books), vec![3, 0]);
+
+        let mut books: BookRecords = books_to_test(vec![3, 2, 0]);
+        let book_title = &books.get(2).unwrap().title.clone();
+        books.remove_book(book_title);
+        books.remove_book("Non-existent title"); // does nothing
+        assert_eq!(collect_weights(&books), vec![3, 2]);
     }
 }
